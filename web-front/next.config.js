3838e8ea0560e8794398285041b021e57b6290eb
yuki <yuki.onishi1129@gmail.com>
2022-10-29 01:54:57 +0900

    add: open api mode

--- web-front/next.config.js
+++ web-front/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+// .env.openapiを呼び出す
+require('dotenv').config({ path: `./.env.${process.env.ENVIRONMENT}` });
+
 const nextConfig = {
   env: {
-    BASE_API_URL: process.env.BASE_API_URL,
+    BASE_API_URL: process?.env?.BASE_OPEN_API_URL ? process.env.BASE_OPEN_API_URL : process.env.BASE_API_URL,
   },
   reactStrictMode: true,
   swcMinify: true,
